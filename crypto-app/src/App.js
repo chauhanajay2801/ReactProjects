7f3856d6acc9b1a8850911270eb3d8b0f7a7e5a0
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-07 16:01:44 +0530

    App structure and route defined

---
+++ crypto-app/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./component/Header";
+import Home from "./component/Home";
+import Exchanges from "./component/Exchanges";
+import Coins from "./component/Coins";
+import CoinDetails from "./component/CoinDetails";
+
+
+
+function App() {
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element ={<Home/>}/>
+        <Route path="/coins" element ={<Coins/>}/>
+        <Route path="/exchanges" element ={<Exchanges/>}/>
+        <Route path="/coin/:id" element ={<CoinDetails/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
