8347d9d4954e4afcc61bb1a18f0c5d1f47f56c45
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-08 21:38:10 +0530

    api added

--- crypto-app/src/component/Exchanges.jsx
+++ crypto-app/src/component/Exchanges.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import axios from 'axios';
+import { server } from '../index';
 
 const Exchanges = () => {
+
+  useEffect(() => {
+
+    const fetchExchange = async()=>{
+      const {data} = await axios.get(`${server}`)
+    }
+  },[])
+
   return (
     <div>Exchanges</div>
   )
