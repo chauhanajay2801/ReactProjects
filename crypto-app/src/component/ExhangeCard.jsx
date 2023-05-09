bff799006f2b4155667168dde0a64577c6f248ce
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-09 15:29:58 +0530

    styling and transition

--- crypto-app/src/component/ExhangeCard.jsx
+++ crypto-app/src/component/ExhangeCard.jsx
@@ -4,7 +4,13 @@ import React from "react";
 const ExhangeCard = ({ name, img, rank, url }) => {
   return (
     <a href={url} target={"blank"}>
-      <VStack>
+      <VStack w={'52'} shadow={'lg'} p={'8'} 
+      borderRadius={'lg'} transition={'all 0.5s'}
+      m={'4'}
+      css={{"&:hover":{
+        transform:"scale(1.1)"
+      }}}
+      >
         <Image
           src={img}
           w={"10"}
