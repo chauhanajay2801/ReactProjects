9b76ee1093fe5da0e9ee854fc0a34ca0c21cff23
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-09 14:44:13 +0530

    mapping done

--- crypto-app/src/component/Exchanges.jsx
+++ crypto-app/src/component/Exchanges.jsx
@@ -1,19 +1,39 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,useState} from 'react';
 import axios from 'axios';
 import { server } from '../index';
+import { Container, HStack } from '@chakra-ui/react';
+import Loader from './Loader';
+
 
 const Exchanges = () => {
 
-  useEffect(() => {
+const [exchanges, setExchanges] = useState([]);
 
+const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
     const fetchExchange = async()=> {
-      const {data} = await axios.get(`${server}`)
-    }
+      const { data } = await axios.get(`${server}/exchanges`);
+      setExchanges(data);
+      setLoading(false);
+      console.log(data);
+    };
+    fetchExchange(); 
   },[])
 
-  return (
-    <div>Exchanges</div>
-  )
+  return <Container maxW={"container.xl"}>
+    {loading? <Loader/> : <>
+    
+    <HStack>
+      {
+        exchanges.map((i)=>(
+          <div>{i.name}</div>
+        ))
+      }
+    </HStack>
+
+    </>}
+  </Container>
 }
 
 export default Exchanges
