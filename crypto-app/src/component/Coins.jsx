bc1c2ff88d97122f8462856604b68db803b3f3ee
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-09 15:44:37 +0530

    coins component added

--- crypto-app/src/component/Coins.jsx
+++ crypto-app/src/component/Coins.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, {useEffect,useState} from 'react';
+import axios from 'axios';
+import { server } from '../index';
+import { Container, HStack } from '@chakra-ui/react';
+import Loader from './Loader';
+import Error from './Error';
+import ExhangeCard from './ExhangeCard';
+
 
 const Coins = () => {
-  return (
-    <div>Coins</div>
-  )
+
+const [exchanges, setExchanges] = useState([]);
+
+const [loading, setLoading] = useState(true)
+
+const [error, setError] = useState(false)
+
+  useEffect(() => {
+    const fetchExchange = async()=> {
+      try{
+        const { data } = await axios.get(`${server}/exchanges`);
+      setExchanges(data);
+      setLoading(false);
+    }
+    catch(error){
+        setLoading(false);
+        setError(true);
+
+      }
+    };
+    fetchExchange(); 
+  },[])
+
+  if(error) return <Error message = {"Error while fetching exchanges"}/>
+
+  return <Container maxW={"container.xl"}>
+    {loading? <Loader/> : <>
+    
+    <HStack wrap={'wrap'}>
+      {
+        exchanges.map((i)=>(
+
+          <ExhangeCard 
+          key={i.id}
+          name={i.name} 
+          img={i.image} 
+          rank={i.trust_score_rank} 
+          url={i.url}/>
+        ))
+      }
+    </HStack>
+
+    </>}
+  </Container>
 }
+
+
 
 export default Coins
