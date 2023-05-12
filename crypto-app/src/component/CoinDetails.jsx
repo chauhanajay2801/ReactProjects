95537ec2e76e6c86d69fecc53e4510a18c309292
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-12 23:14:25 +0530

    coin details 2

--- crypto-app/src/component/CoinDetails.jsx
+++ crypto-app/src/component/CoinDetails.jsx
@@ -1,27 +1,46 @@
-import { Box, Container } from '@chakra-ui/react'
-import {React, useState} from 'react'
-import Loader from './Loader';
+import axios from "axios";
+import Loader from "./Loader";
+import { server } from "../index";
+import { useParams } from "react-router-dom";
+import { Box, Container } from "@chakra-ui/react";
+import { React, useState, useEffect } from "react";
 
 const CoinDetails = () => {
-  
   const [coin, setCoin] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [currency, setCurrency] = useState("inr");
-  
-  return 
-  <Container maxW={'container.xl'}>
+  const params = useParams()
+
+
+  useEffect(() => {
+    const fetchCoin = async () => {
+      try {
+        const { data } = await axios.get(
+          `${server}/coins/${params.id}`
+        );
+        setCoin(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchCoins();
+  }, [currency, page]);
+
+  return;
+  <Container maxW={"container.xl"}>
     {loading ? (
-    <Loader/>
-    ):(
+      <Loader />
+    ) : (
       <>
-      <Box width={'full'} borderWidth={'1'}>
-heyttttt
-      </Box>
+        <Box width={"full"} borderWidth={"1"}>
+          heyttttt
+        </Box>
       </>
-      )}
+    )}
+  </Container>;
+};
 
-  </Container>
-}
-
-export default CoinDetails
+export default CoinDetails;
