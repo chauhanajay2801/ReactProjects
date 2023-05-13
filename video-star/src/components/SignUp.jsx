83d832251034f5d0351e35e8652eacfe5bcd196f
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-13 23:37:51 +0530

    Sign Up Component completed

--- video-star/src/components/SignUp.jsx
+++ video-star/src/components/SignUp.jsx
@@ -1,9 +1,36 @@
+import { Avatar, Button, Container, Heading, Input, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <Container maxW={'container.xl'} h={'110vh'} p={'16'}>
+
+        <form action="">
+            <VStack alignItems={'stretch'} spacing={'8'} w={['full','96']} m={'auto'} my={'16'}>
+                <Heading alignSelf={'center'}>Video-Star</Heading>
+                <Avatar alignSelf={'center'} boxSize={'32'}/>
+
+                <Input placeholder='Name' type='text' required focusBorderColor='blue.500'/>
+                <Input placeholder='Email' type='Email' required focusBorderColor='blue.500'/>
+                <Input placeholder='Password' type='Password' required focusBorderColor='blue.500'/>
+             
+
+            <Button colorScheme='blue' type='submit'>Sign Up</Button>
+
+            <Text textAlign={'right'}>Already Signed Up?{" "}
+            <Button variant={'link'} colorScheme='blue'>
+                    <Link to = {"/login"}>Login</Link>
+                </Button>
+            </Text>
+           
+
+            </VStack>
+        </form>
+
+    </Container>
   )
 }
+
 
 export default SignUp
