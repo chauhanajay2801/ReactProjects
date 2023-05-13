888cc64e1d5891a0998df2d7495c6df95f82fa47
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-13 18:00:32 +0530

    overlay fixed

--- video-star/src/components/Header.jsx
+++ video-star/src/components/Header.jsx
@@ -29,6 +29,7 @@ const Header = () => {
         h={'10'}
         borderRadius={'full'}
         onClick={onOpen}
+        zIndex={'overlay'}
       >
         <BiMenuAltLeft size={'20'} />
       </Button>
---
+++ video-star/src/components/Videos.jsx
@@ -0,0 +1,14 @@
+import { Stack } from '@chakra-ui/react';
+import React from 'react'
+
+const Videos = () => {
+  return (
+   <Stack
+   direction={['column','row']} h={'100vh'}
+   >
+
+   </Stack>
+  );
+}
+
+export default Videos
