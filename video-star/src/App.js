1a6d6aec96d6d45836809eb433da861a44af9436
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-13 20:26:18 +0530

    upload 2

--- video-star/src/App.js
+++ video-star/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Videos from './components/Videos';
+import Upload from './components/Upload';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/videos' element={<Videos/>} />
+        <Route path='/upload' element={<Upload/>} />
       </Routes>
       <Footer/>
     </Router>
--- video-star/src/components/Upload.jsx
+++ video-star/src/components/Upload.jsx
@@ -1,18 +1,15 @@
-import { Container, VStack } from '@chakra-ui/react'
-import React from 'react'
-import { AiOutlineCircleCloudUpload } from 'react-icons/ai'
+import { Container, VStack } from '@chakra-ui/react';
+import React from 'react';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
 
 const Upload = () => {
   return (
     <Container maxW={'container.xl'} h={'100vh'} p={'16'}>
-
-<VStack color={'blue.500'} h={'full'} justifyContent={'center'}>
-
-    <AiOutlineCircleCloudUpload size={'10vmax'}/>
-
-</VStack>
+      <VStack color={'blue.500'} h={'full'} justifyContent={'center'}>
+        <AiOutlineCloudUpload size={'10vmax'} />
+      </VStack>
     </Container>
-  )
-}
+  );
+};
 
-export default Upload
+export default Upload;
