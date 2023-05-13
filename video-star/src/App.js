63a9a066df82d69c0e92f0d9a3656fd19d4c338e
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-13 11:51:28 +0530

    video app

---
+++ video-star/src/App.js
@@ -0,0 +1,15 @@
+import { Button } from '@chakra-ui/react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
