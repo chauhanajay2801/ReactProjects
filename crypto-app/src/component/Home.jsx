0e08d1aaf66028ee1caee13b02901311b38ae73d
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-15 15:12:33 +0530

    Home done

--- crypto-app/src/component/Home.jsx
+++ crypto-app/src/component/Home.jsx
@@ -1,14 +1,21 @@
-import { Box, Image, Img, Text } from '@chakra-ui/react'
-import React from 'react'
-import btsrr from "../assets/btc.png"
+import { Box, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import image1 from "../assets/pexels-worldspectrum-1097946 (1).jpg";
 
 const Home = () => {
   return (
-    <Box bgColor={'blackAlpha.900'} w={'full'} h={'85vh'}>
-      <Img w={'full'} h={'full'} objectFit={'contain'} srcSet='btsrr'/>
-      <Text fontSize={'6xl'} textAlign={'center'} fontWeight={'thin'} color={'whiteAlpha.700'} >CryptoWin</Text>
+    <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
+      <Image w={"full"} h={"full"} objectFit={"contain"} src={image1} />
+      <Text
+        fontSize={"6xl"}
+        textAlign={"center"}
+        fontWeight={"thin"}
+        color={"whiteAlpha.700"}
+      >
+        CryptoWin
+      </Text>
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
