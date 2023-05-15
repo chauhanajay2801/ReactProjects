e2be992b67a0d3ec27d8959a6c5318b0e960c3a6
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-15 16:08:01 +0530

    Footer1

---
+++ crypto-app/src/component/Footer.jsx
@@ -0,0 +1,16 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <Box
+   bgColor={'blackAlpha.900'}
+   color={'whiteAlpha.700'}
+    minH={'48'}
+    px={'16'}
+    py={['16','8']}
+   ></Box>
+  )
+}
+
+export default Footer
